672f749c9a71b54dec41c6e74780652258195d22
Aricelio de Souza Fernandes <a.de.souza.fernandes@avanade.com>
2022-01-28 21:52:56 -0300

    Aula 02 - Aplicação de States

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle(){
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Titulo(props){    
     const Tag = props.tag || "h1";
@@ -46,24 +19,13 @@ function Titulo(props){
     );
 }
 
-// Componente React
-//function HomePage() {    
-//JSX
-// return (
-//     <div>
-//         <GlobalStyle></GlobalStyle>
-//         <Titulo tag="h2">Boas vindas de volta!</Titulo>    
-//         <h2>Discord - Alura Matrix</h2>            
-//     </div>        
-// )
-//}
-//export default HomePage
 export default function PaginaInicial() {
-    const username = 'Aricelio';
+    //const username = 'Aricelio';
+    const [username, setUsername] = React.useState('Aricelio');
+    const roteamento = useRouter();
   
     return (
-      <>
-        <GlobalStyle />
+      <>        
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +53,12 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={ function(infosDoEvento){
+                infosDoEvento.preventDefault();
+                console.log("Form submetido");
+                //window.location.href = '/chat';
+                roteamento.push('/chat');
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,7 +69,39 @@ export default function PaginaInicial() {
                 {appConfig.name}
               </Text>
   
+              {/*
+              <input 
+                type="text" 
+                value={username}
+                onChange={function handler(event){
+                  console.log("Usuario digitou: ", event.target.value)
+                  
+                  // Valor
+                  const valor = event.target.value;
+
+                  // Troca o valor da variavel através do React
+                  setUsername(valor);
+
+                }}
+              />
+              */}
+
               <TextField
+                value={username}
+                onChange={function handler(event){
+                  //console.log("Usuario digitou: ", event.target.value)
+                  
+                  // Valor
+                  const valor = event.target.value;
+
+                  //const tamanho = valor.size;
+                  //console.log("Tamanho: ", tamanho);
+
+                  // Troca o valor da variavel através do React
+                  setUsername(valor);
+                  
+                }}
+
                 fullWidth
                 textFieldColors={{
                   neutral: {
@@ -111,7 +111,8 @@ export default function PaginaInicial() {
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
                 }}
-              />
+              />              
+
               <Button
                 type='submit'
                 label='Entrar'
@@ -167,4 +168,17 @@ export default function PaginaInicial() {
         </Box>
       </>
     );
-  }
+}
+
+// Componente React
+//function HomePage() {    
+//JSX
+// return (
+//     <div>
+//         <GlobalStyle></GlobalStyle>
+//         <Titulo tag="h2">Boas vindas de volta!</Titulo>    
+//         <h2>Discord - Alura Matrix</h2>            
+//     </div>        
+// )
+//}
+//export default HomePage
